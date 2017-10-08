e324b6d69f6b096eac74f28690b4455633a916f7
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-08 17:02:42 +0300

    Shift class into folder

---
+++ mediateka/classes/picture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Interfaces;
+
+namespace Mediateka.Classes
+{
+    public class Picture : MediaItem, IPicture
+    {
+        public string Name { get; private set; }
+
+        public Size Size => throw new NotImplementedException();
+
+        public Rating Rating => throw new NotImplementedException();
+
+        public StreamReader MediaStream => throw new NotImplementedException();
+
+        public long PhysicalSize => throw new NotImplementedException();
+
+        public DateTime CreationTime => throw new NotImplementedException();
+
+        public Picture(string name) : base(name)
+        {
+            Name = name;
+        }
+    }
+}
