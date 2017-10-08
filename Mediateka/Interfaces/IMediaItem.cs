027c9f4e3a46d05e50355f64a2314b3b48de2744
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-08 13:04:37 +0300

    Добавьте файлы проекта.

---
+++ Mediateka/Interfaces/IMediaItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Classes;
+using System.IO;
+
+namespace Mediateka.Interfaces
+{
+    public interface IMediaItem : INameable
+    {
+        Rating Rating { get; }
+        StreamReader MediaStream { get; }
+        Int64 PhysicalSize { get; }
+        DateTime CreationTime { get; }
+    }
+}
---
+++ Mediateka/Interfaces/IPicture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Interfaces
+{
+    interface IPicture: INameable , IMediaItem
+    {
+        System.Drawing.
+    }
+}
