dd052bb483a2f9fe3a367242bb6558798e7be9ed
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-09 16:33:14 +0300

    Revert "Revert "Released part of Player and Medioteka classes.""
    This reverts commit ee2a325f0db4dc84a037800b123338bcb32822a6.

---
+++ Mediateka/Classes/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Classes;
+using Mediateka.Interfaces;
+using System.Media;
+
+namespace Mediateka.Classes
+{
+    public class Player
+    {
+        SoundPlayer soundPlayer = new SoundPlayer();
+   
+        public Player(IPlayList playList)
+        {
+            Console.Clear();
+            int switchCase = 2;
+            foreach (IMediaItem item in playList.Items)
+            {
+
+                Console.WriteLine("1. Запуск проигрывания");
+                Console.WriteLine("2. Остановка проигрывания");
+                soundPlayer.Stream = item.MediaStream;
+                while (switchCase != 3)
+                {
+                    switchCase = Int32.Parse(Console.ReadLine());
+                    switch (switchCase)
+                    {
+                        case 1: soundPlayer.PlaySync(); break;
+                        case 2: soundPlayer.Stop(); break;
+                        case 3: break;
+                        default: break;
+                    }
+                }
+            }
+        }
+
+        public Player(Mediateka playList)
+        {
+            int switchCase = 1;
+            foreach (IMediaItem item in playList)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Запуск проигрывания");
+                Console.WriteLine("2. Остановка проигрывания");
+                Console.WriteLine("3. Выход из текущей песни");
+                Console.WriteLine(item.Name + item.Rating);
+                soundPlayer.Stream = item.MediaStream;
+                while (switchCase != 3)
+                {
+                    switchCase = Int32.Parse(Console.ReadLine());
+                    switch (switchCase)
+                    {
+                        case 1: soundPlayer.PlaySync(); break;
+                        case 2: soundPlayer.Stop(); break;
+                        case 3: break;
+                        default: break;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- Mediateka/Program.cs
+++ Mediateka/Program.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Mediateka.Classes;
 using Mediateka.Interfaces;
+using System.Media;
+using WMPLib;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
 
 namespace Mediateka
 {
@@ -13,14 +18,26 @@ namespace Mediateka
         static void Main(string[] args)
         {
             Mediateka myMediateka = new Mediateka();
-            IMediaItem picture = new WebPicture("first", "networld");
-            IMediaItem track = new DiscTrack("look what u made me do","pop", "U SHALL NO PATH");
-            myMediateka.Add(picture);
-            myMediateka.Add(track);
+            IMediaItem picture = new DiscPicture("first", "E:\\picture1.jpg");
+            IMediaItem track = new DiscTrack("look what u made me do","pop", "E:\\track1.mp3");
+            IMediaItem track2 = new DiscTrack("kek","book" ,"E:\\track1.wav");
+            //myMediateka.Add(picture);
+            myMediateka.Add(track2);
+            myMediateka.Add(track2);
             foreach(var temp in myMediateka)
             {
                 Console.WriteLine(temp.Name);
             }
+
+            Player myPlayer = new Player(myMediateka);
+            //WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+            //wplayer.URL = "E:\\track1.mp3";
+            ////wplayer.controls.play();
+            //SoundPlayer mySoundPlayer = new SoundPlayer();
+            //mySoundPlayer.Stream = track2.MediaStream;
+            //mySoundPlayer.PlaySync();
+            Console.ReadKey();
+            
         }
     }
 }
